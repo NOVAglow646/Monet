455f0b3911ea3a28371b19051b0328eb7484092d
NOVAglow646 <190001925@pku.edu.cn>
2025-10-02 22:36:25 +0800

    10.2

--- sft_eval_obs_acc.sh
+++ sft_eval_obs_acc.sh
@@ -13,8 +13,8 @@ python -m src.sft_eval_obs_acc_non_dist \
     "./new/created_dataset/filtered_data/Zebra_CoT_visual_search/filtered_train_w_metadata_9.24_further_washed.json" \
   "./new/created_dataset/filtered_data/CoM_w_MathVista/filtered_train_w_metadata_9.1.json" \
   "./new/created_dataset/filtered_data/ReFocus/filtered_train_w_metadata_9.1.json" \
-  "./new/created_dataset/filtered_data/Zebra_CoT_count/filtered_train_w_metadata_9.25_max_seq_len4096_max_seq_len3000.json" \
+  "./new/created_dataset/filtered_data/Zebra_CoT_count/filtered_train_w_metadata_9.25_max_seq_len4096_max_seq_len3000_max_seq_len2500.json" \
     --log_file "./log.txt" \
-    --load_model_path /home/dids/shiyang/checkpoints/after9.28/9.27_avt_sft_wo_maze_ce2.0.zip/9.27_avt_sft_wo_maze_ce2.0
+    --load_model_path /home/dids/shiyang/checkpoints/after9.28/10.1_avt_sft_wo_maze_ce2.0/10.1_avt_sft_wo_maze_ce2.0
 
 
